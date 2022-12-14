c0db98e1508d90a56efa4e11316ffcbb9b9f8249
NahuRodas <cesarnahuelrodas@gmail.com>
2022-12-14 18:59:05 -0300

    14/12

---
+++ codigo.js
@@ -0,0 +1,20 @@
+/* console.clear();
+
+var video = [document.getElementsByClassName('videos')];
+
+video.forEach(e => {
+    console.log(e.atributes);
+});
+
+ // Funcion que genera un codigo de video aleatorio
+function generador() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        codigoVideo = ""; 
+
+    for (let i = 0; i < 11; i++) {
+        var numeroRandom = Math.floor(Math.random() * chars.length);
+        codigoVideo += chars.substring(numeroRandom, numeroRandom + 1);
+    }
+    return "https://www.youtube.com/watch?v=" + codigoVideo;
+}
+ */
